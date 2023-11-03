a9d9dba8591a84d33ba684b535dba0fd91537b29
Akuna Joshua <akunajoshua@gmail.com>
2023-11-03 17:59:11 +0100

    task 2. Select some Amenities to be comfortable! : files added

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,38 @@
+const checkedIds = [];
+const checkedNames = [];
+
+$( window ).on('load', ()=> {
+	$('.amenity').change(function() {
+
+		const amenityId = $(this).data('id');
+		const amenityName = $(this).data('name');
+		// console.log(id);
+
+		if ($(this).is(':checked')){
+			if (!(checkedIds.includes(amenityId))){
+				checkedIds.push(amenityId);
+				checkedNames.push(amenityName);
+			}
+		} else {
+			if (checkedIds.includes(amenityId)){
+				index = checkedIds.indexOf(amenityId);
+				checkedIds.splice(index, 1)
+				index = checkedNames.indexOf(amenityName);
+				checkedNames.splice(index, 1)
+			}
+		}
+		if (checkedNames.length > 0){
+			text = "";
+			for (i in checkedNames.sort()) {
+				if (i > 0)
+					text += ", ";
+				text += checkedNames[i];
+			}
+			$('div.amenities h4').text(text);
+			$('div.amenities h4').css('overflow', 'hidden');
+		} else {
+			$('div.amenities h4').html('&nbsp;');
+			$('div.amenities h4').css('overflow', 'inherit');
+		}
+	});
+});
