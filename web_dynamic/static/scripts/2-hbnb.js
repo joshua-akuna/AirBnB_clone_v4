66a41e0de54fb09e7e596e9df410c3632a57b6e1
Akuna Joshua <akunajoshua@gmail.com>
2023-11-04 23:57:43 +0100

    web_dynamic/static/scripts/2-hbnb.js : correction : semistandard

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,68 +1,64 @@
 const checkedIds = [];
 const checkedNames = [];
 
-$( window ).on('load', ()=> {
+$(window).on('load', () => {
+  setInterval(changeApiStatus, 3000);
 
-	setInterval(changeApiStatus, 3000);
+  $('.amenity').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+    // console.log(id);
 
-	$('.amenity').change(function() {
+    if ($(this).is(':checked')) {
+      if (!(checkedIds.includes(amenityId))) {
+        checkedIds.push(amenityId);
+        checkedNames.push(amenityName);
+      }
+    } else {
+      if (checkedIds.includes(amenityId)) {
+        let index = checkedIds.indexOf(amenityId);
+        checkedIds.splice(index, 1);
+        index = checkedNames.indexOf(amenityName);
+        checkedNames.splice(index, 1);
+      }
+    }
+    if (checkedNames.length > 0) {
+      let text = '';
+      for (const i in checkedNames.sort()) {
+        if (i > 0) { text += ', '; }
+        text += checkedNames[i];
+      }
+      $('div.amenities h4').text(text);
+      $('div.amenities h4').css('overflow', 'hidden');
+    } else {
+      $('div.amenities h4').html('&nbsp;');
+      $('div.amenities h4').css('overflow', 'inherit');
+    }
+  });
 
-		const amenityId = $(this).data('id');
-		const amenityName = $(this).data('name');
-		// console.log(id);
+  function changeApiStatus () {
+    const status = getStatus();
+    if (status === 'OK') {
+      if (!$('div#api_status').hasClass('available')) {
+        $('div#api_status').addClass('available');
+      }
+    } else {
+      if ($('div#api_status').hasClass('available')) {
+        $('div#api_status').removeClass('available');
+      }
+    }
+  }
 
-		if ($(this).is(':checked')){
-			if (!(checkedIds.includes(amenityId))){
-				checkedIds.push(amenityId);
-				checkedNames.push(amenityName);
-			}
-		} else {
-			if (checkedIds.includes(amenityId)){
-				index = checkedIds.indexOf(amenityId);
-				checkedIds.splice(index, 1)
-				index = checkedNames.indexOf(amenityName);
-				checkedNames.splice(index, 1)
-			}
-		}
-		if (checkedNames.length > 0){
-			text = "";
-			for (i in checkedNames.sort()) {
-				if (i > 0)
-					text += ", ";
-				text += checkedNames[i];
-			}
-			$('div.amenities h4').text(text);
-			$('div.amenities h4').css('overflow', 'hidden');
-		} else {
-			$('div.amenities h4').html('&nbsp;');
-			$('div.amenities h4').css('overflow', 'inherit');
-		}
-	});
-
-	function changeApiStatus() {
-		const status = getStatus();
-		if (status == 'OK')
-		{
-			if(!$('div#api_status').hasClass('available')){
-				$('div#api_status').addClass('available');
-			}
-		} else {
-			if ($('div#api_status').hasClass('available')){
-				$('div#api_status').removeClass('available');
-			}
-		}
-	}
-	
-	function getStatus() {
-		let status = null;
-		$.ajax({
-			url: "http://127.0.0.1:5001/api/v1/status/",
-			type: 'get',
-			async: false,
-			success: function(data) {
-				status = data.status;
-			}
-		});
-		return status;
-	}
+  function getStatus () {
+    let status = null;
+    $.ajax({
+      url: 'http://127.0.0.1:5001/api/v1/status/',
+      type: 'get',
+      async: false,
+      success: function (data) {
+        status = data.status;
+      }
+    });
+    return status;
+  }
 });
