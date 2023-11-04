e50e98176066defd5e392393c7d74b1fba767f8f
Akuna Joshua <akunajoshua@gmail.com>
2023-11-04 23:56:49 +0100

    web_dynamic/static/scripts/1-hbnb.js : correction : semistandard

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,38 +1,36 @@
 const checkedIds = [];
 const checkedNames = [];
 
-$( window ).on('load', ()=> {
-	$('.amenity').change(function() {
+$(window).on('load', () => {
+  $('.amenity').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+    // console.log(id);
 
-		const amenityId = $(this).data('id');
-		const amenityName = $(this).data('name');
-		// console.log(id);
-
-		if ($(this).is(':checked')){
-			if (!(checkedIds.includes(amenityId))){
-				checkedIds.push(amenityId);
-				checkedNames.push(amenityName);
-			}
-		} else {
-			if (checkedIds.includes(amenityId)){
-				index = checkedIds.indexOf(amenityId);
-				checkedIds.splice(index, 1)
-				index = checkedNames.indexOf(amenityName);
-				checkedNames.splice(index, 1)
-			}
-		}
-		if (checkedNames.length > 0){
-			text = "";
-			for (i in checkedNames.sort()) {
-				if (i > 0)
-					text += ", ";
-				text += checkedNames[i];
-			}
-			$('div.amenities h4').text(text);
-			$('div.amenities h4').css('overflow', 'hidden');
-		} else {
-			$('div.amenities h4').html('&nbsp;');
-			$('div.amenities h4').css('overflow', 'inherit');
-		}
-	});
+    if ($(this).is(':checked')) {
+      if (!(checkedIds.includes(amenityId))) {
+        checkedIds.push(amenityId);
+        checkedNames.push(amenityName);
+      }
+    } else {
+      if (checkedIds.includes(amenityId)) {
+        let index = checkedIds.indexOf(amenityId);
+        checkedIds.splice(index, 1);
+        index = checkedNames.indexOf(amenityName);
+        checkedNames.splice(index, 1);
+      }
+    }
+    if (checkedNames.length > 0) {
+      let text = '';
+      for (const i in checkedNames.sort()) {
+        if (i > 0) { text += ', '; }
+        text += checkedNames[i];
+      }
+      $('div.amenities h4').text(text);
+      $('div.amenities h4').css('overflow', 'hidden');
+    } else {
+      $('div.amenities h4').html('&nbsp;');
+      $('div.amenities h4').css('overflow', 'inherit');
+    }
+  });
 });
